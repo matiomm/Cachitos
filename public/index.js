552224a2414b7fb78a4acbd5ffccb4b124a611e2
matiomm <matimoscoso@gmail.com>
2021-07-11 14:31:21 -0400

    pruebas

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ function setup() {
     background(0);
     // Start a socket connection to the server
     // Some day we would run this server somewhere else
-    socket = io.connect('http://localhost:3000');
+    socket = io.connect('http://45.56.122.6');
     // We make a named event called 'mouse' and write an
     // anonymous callback function
     socket.on('mouse',
