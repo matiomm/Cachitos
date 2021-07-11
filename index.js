5fb66f74b7b96b38fdc8aae833d8e92c5be20712
matiomm <matimoscoso@gmail.com>
2021-07-11 02:23:18 -0400

    huge cambios

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const io = require('socket.io')();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
